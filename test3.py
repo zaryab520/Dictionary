4d5e37def138aad93236b078f2bba0063a4c1afa
zaryab520 <hakeemzaryab@gmail.com>
2020-09-16 04:31:53 -0700

    First commit

---
+++ test3.py
@@ -0,0 +1,42 @@
+import xlsxwriter
+import openpyxl
+
+
+def newdic():
+    
+    ''' newdic() stands for new dictionary, it adds new word to the dictionary. The function
+        asks Dari and Pashto translation of the word from the user.
+
+        test5.xlsx is the database that contains words.
+     '''
+
+    newword = input('Please enter the word you want to add: ')
+    NewWord = newword.upper()
+
+    
+    wb = openpyxl.load_workbook('test5.xlsx')
+    ws = wb.active
+    row = ws.max_row
+    column = ws.max_column
+
+    # using for loop it iterates all the rows in Database and matches the word
+    for row in ws.iter_rows(ws.min_row, ws.max_row):
+        for cell in row:
+            if cell.value == NewWord:
+                print('This word already exists')
+                newdic()
+                break
+
+    Dari_trans = input('Enter the Dari Translation of the word: ')
+    pashot_trans = input('Enter the Pashto Translation of the word: ')
+
+    wb = openpyxl.load_workbook('test5.xlsx')
+    ws = wb.active
+    row = ws.max_row
+    column = ws.max_column
+
+    dic_list = [NewWord,Dari_trans,pashot_trans]
+    for value in dic_list:
+        ws.cell(row+1, column-5).value = value
+        column+=1
+    wb.save('test5.xlsx')
---
+++ test4.py
@@ -0,0 +1,84 @@
+import xlsxwriter
+import openpyxl
+import test3
+
+
+
+print('Dictionary'.center(100,'='))
+print(''' 
+Welcome to Dictionary application you can use to translate English word to Dari & Pashto 
+''')
+
+rowpos = 0
+colpos = 0
+
+def dic():
+
+    """ dic() function first ask the user for the English word, then it prompt for the 
+        language that user needs the word to be translated to (Dari/Pashto).
+
+        It also provide to users the ability to add new translation to dictionary if it doesnt
+        exists.
+    
+     """
+    english_word = input('Please enter the word to Translate: ')
+    English_Word = english_word.upper()
+
+    while True:
+        d1a = input ("Please Choose the Language to Translate: A) Dari. B) Pashto. [A/B]? : ")
+    
+        if d1a in ['A', 'B']:
+            break
+
+    if d1a == "A": 
+        wb = openpyxl.load_workbook('test5.xlsx') 
+        ws = wb['emailcontent']
+        flag = False
+        for row in ws.iter_rows(ws.min_row, ws.max_row):
+            for cell in row:
+                if cell.value == English_Word:
+                    rowpos = cell.row
+                    colpos = cell.column
+                    print(ws.cell(rowpos, colpos+1).value)
+                    flag = True
+                    break
+            if flag:
+                break
+        else:
+            userval = input('No such word found. Do you want to add the word in the Dictionary? (y/n): ')
+            if userval == 'y':
+                import test3
+                test3.newdic()
+            else:
+                print('thanks for using our product')
+
+
+
+
+    elif d1a == "B": 
+        wb = openpyxl.load_workbook('test5.xlsx')
+        ws = wb['emailcontent']
+        flag = False
+        for row in ws.iter_rows(ws.min_row, ws.max_row):
+            for cell in row:
+                if cell.value == English_Word:
+                    rowpos = cell.row 
+                    colpos = cell.column
+                    print(ws.cell(rowpos, colpos+2).value)
+                    flag = True
+                    break
+            if flag:
+                break
+        else:
+            userval = input('No such word found. Do you want to add the word in the Dictionary? (y/n): ')
+            if userval == 'y':
+                
+                test3.newdic()
+                
+            else:
+                print('Thank you for using our product')                   
+dic()
+
+
+
+
